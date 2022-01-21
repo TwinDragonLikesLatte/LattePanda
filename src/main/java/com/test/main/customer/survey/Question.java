a55289071b44836a7ca65f918de27c13677ba951
Jinn <72987464+Dev-Jinuk-Jo@users.noreply.github.com>
2022-01-22 06:22:56 +0900

    [Feat] : Add servlet and JSP for nav test
    Add servlet and JSP for nav's link and function test

---
+++ src/main/java/com/test/main/customer/survey/Question.java
@@ -0,0 +1,20 @@
+package com.test.main.customer.survey;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/customer/survey/question.do")
+public class Question extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/customer/survey/question.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
