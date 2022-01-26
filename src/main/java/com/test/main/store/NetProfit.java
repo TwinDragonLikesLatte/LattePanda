a134bca634b8089859408a44cbcca9c888c2d495
Kang Kujun <donato14@naver.com>
2022-01-27 02:21:34 +0900

    [Feat](매장관리): Add 순이익 파트 구성 추가

--- src/main/java/com/test/main/store/NetProfit.java
+++ src/main/java/com/test/main/store/NetProfit.java
@@ -1,6 +1,7 @@
 package com.test.main.store;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +10,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.main.home.MontlyTotalDTO;
+
 @WebServlet("/store/netprofit/net.do")
 public class NetProfit extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    	resp.setCharacterEncoding("UTF-8");
+    	
+    	String store = req.getParameter("store");
+    	
+    	if(store == null) {
+    		store = "11801";
+    	}
+    	
+    	System.out.println(store);
+    	
+    	//[세션에서 불러들일 값]
+    	//String seq_position = (String) session.getAttribut("seq_position");
+    	//position 점장(8), [(1),(2),(3),(4),(7) 지역장, 과장이상], 매니저(9), 사원(6), 대리(5)
+    	
+    	String seq_position = "7";
+    	
+    	StoreDAO dao = new StoreDAO();
+    	
+    	ArrayList<StoreProfitDTO> storeprofit = dao.storeprofit();
+    	ArrayList<ProfitValuesDTO> profitvalues = dao.profitvalues(store);
+    	
+    	req.setAttribute("storeprofit", storeprofit);
+    	req.setAttribute("profitvalues", profitvalues);
+    	req.setAttribute("store", store);
+    	
+    	req.setAttribute("seq_position", seq_position);
+    	
+    	
+    	dao.close();
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/store/netprofit.jsp");
         dispatcher.forward(req, resp);
     }
--- src/main/webapp/WEB-INF/inc/sub-nav_store.jsp
+++ src/main/webapp/WEB-INF/inc/sub-nav_store.jsp
@@ -12,20 +12,20 @@
                 <li><a href="/store/netprofit/operating.do">영업이익별 순위</a></li>
             </ul>
         </li>
-        <li class="sub-menu"><a href="/store/goal.do" class="">지점별 목표설정</a>
+        <!-- <li class="sub-menu"><a href="/store/goal.do" class=""></a>
             <ul>
                 <li><a href="">서브서브메뉴1</a></li>
                 <li><a href="">서브서브메뉴2</a></li>
                 <li><a href="">서브서브메뉴3</a></li>
             </ul>
         </li>
-        <li class="sub-menu"><a href="/store/lossratio.do" class="">지점별 로스율</a>
+        <li class="sub-menu"><a href="/store/lossratio.do" class=""></a>
             <ul>
                 <li><a href="">서브서브메뉴1</a></li>
                 <li><a href="">서브서브메뉴2</a></li>
                 <li><a href="">서브서브메뉴3</a></li>
             </ul>
-        </li>
+        </li> -->
     </ul>
 </nav>
 
