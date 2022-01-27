f3eedb7e19200c3cb287e2e7f486b508fb63d4fe
Jinn <72987464+Dev-Jinuk-Jo@users.noreply.github.com>
2022-01-28 08:45:57 +0900

    [Feat](HR): Add Employee list view and functions
    Add view employee list
    Add func dynamic employee list by org-chart

---
+++ src/main/java/com/test/main/hr/employee/EmployeeDTO.java
@@ -0,0 +1,95 @@
+package com.test.main.hr.employee;
+
+public class EmployeeDTO {
+
+    private String seqEmployee;
+    private String name;
+    private String tel;
+    private String mail;
+    private String department;
+    private String position;
+    private String join;
+    private String status;
+    private String seqDepartment;
+    private String seqPositiion;
+
+    public String getSeqEmployee() {
+        return seqEmployee;
+    }
+
+    public void setSeqEmployee(String seqEmployee) {
+        this.seqEmployee = seqEmployee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getJoin() {
+        return join;
+    }
+
+    public void setJoin(String join) {
+        this.join = join;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSeqDepartment() {
+        return seqDepartment;
+    }
+
+    public void setSeqDepartment(String seqDepartment) {
+        this.seqDepartment = seqDepartment;
+    }
+
+    public String getSeqPositiion() {
+        return seqPositiion;
+    }
+
+    public void setSeqPositiion(String seqPositiion) {
+        this.seqPositiion = seqPositiion;
+    }
+}
